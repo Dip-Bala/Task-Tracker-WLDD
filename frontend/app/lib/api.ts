a959181445a0e38b06840f135e57de5ccd509e75
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2025-12-27 17:33:23 +0530

    next config file is updated for production

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-if(!process.env.NEXT_PUBLIC_BACKEND_URL){
-  throw Error("Backend url is not loaded from env")
-}
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: '/api',
   withCredentials: true,
 });
+
+process.env.NEXT_PUBLIC_BACKEND_URL
